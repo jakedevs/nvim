9f2a9bd2f201630d70ee6bb714c0de0cd1c6db4d
jakedevs <jakedevquestions@gmail.com>
2024-01-18 17:37:11 -0500

    Remove luasnip, use lsp snippets instead

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,15 +3,11 @@ return {
   { "hrsh7th/cmp-cmdline" },
   { "hrsh7th/cmp-nvim-lsp" },
   { "hrsh7th/cmp-buffer" },
-  { "saadparwaiz1/cmp_luasnip" },
   {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
       cmp.setup({
-        snippet = {
-          expand = function(args) require("luasnip").lsp_expand(args.body) end,
-        },
 
         mapping = cmp.mapping.preset.insert({
           ["<C-Space>"] = cmp.mapping.complete(),
@@ -24,9 +20,8 @@ return {
           documentation = cmp.config.window.bordered(),
         },
         sources = cmp.config.sources({
-          { name = "luasnip" },
           { name = "path" },
-          -- { name = "nvim_lsp" }, 
+          { name = "nvim_lsp" },
         }, {
           { name = "buffer" },
         }),
@@ -35,6 +30,17 @@ return {
       cmp.setup.cmdline(":", {
         mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
+      })
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      require("lspconfig").tsserver.setup({
+        capabilities = capabilities,
+      })
+      require("lspconfig").cssls.setup({
+        capabilities = capabilities,
+      })
+      require("lspconfig").html.setup({
+        capabilities = capabilities,
       })
     end,
   },
