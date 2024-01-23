82e1ae48ee8b584d523e7a5fe23aecaf52af691a
jakedevs <jakedevquestions@gmail.com>
2024-01-23 18:13:38 -0500

    Testing switching to rocks.nvim plugin manager

--- init.lua
+++ init.lua
@@ -1,24 +1,20 @@
-vim.g.mapleader = " "
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins", { change_detection = { enabled = false } })
-
--- Currently active colorscheme
--- See plugins/colorschemes for colorscheme list
-vim.cmd("colorscheme kanagawa")
-
-require("mappings")
-require("options")
+ local rocks_config = {
+     rocks_path = "/home/jake/.local/share/nvim/rocks",
+     luarocks_binary = "/home/jake/.local/share/nvim/rocks/bin/luarocks",
+ }
+ 
+ vim.g.rocks_nvim = rocks_config
+ 
+ local luarocks_path = {
+     vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
+     vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
+ }
+ package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
+ 
+ local luarocks_cpath = {
+     vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
+     vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
+ }
+ package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
+ 
+ vim.opt.runtimepath:append(vim.fs.joinpath(rocks_config.rocks_path, "lib", "luarocks", "rocks-5.1", "rocks.nvim", "*"))                                                        
--- lua/plugins/toggleterm.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  config = function()
-    require("toggleterm").setup()
-    -- Keymap
-    vim.keymap.set("n", "<leader>tt", ":ToggleTerm direction=float<cr>", { desc = "toggle terminal" })
-  end,
-}
