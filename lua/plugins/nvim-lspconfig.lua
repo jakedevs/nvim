10fba4b86714add7b5b443e328c70bd496714aa4
jakedevs <jakedevquestions@gmail.com>
2024-01-23 04:20:27 -0500

    Add ltex-ls, add zen-mode

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,34 +1,37 @@
 return {
 
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function() require("mason").setup() end,
+  },
 
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls" },
-			})
-		end,
-	},
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" },
+      })
+    end,
+  },
 
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		dependencies = { "folke/neodev.nvim", opts = {} },
-		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.html.setup({})
-			lspconfig.cssls.setup({})
-			lspconfig.tsserver.setup({})
-			settings = {}
-		end,
-	},
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    dependencies = { "folke/neodev.nvim", opts = {} },
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.html.setup({})
+      lspconfig.cssls.setup({})
+      lspconfig.tsserver.setup({})
+      lspconfig.ltex.setup({ settings = {
+        ltex = {
+          language = "en-US",
+        },
+      } })
+      settings = {}
+    end,
+  },
 }
