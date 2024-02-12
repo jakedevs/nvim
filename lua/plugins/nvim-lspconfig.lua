0fd8cd8ccf3ebd92c9f1cd241d7d9f85d6c89158
Jake Levi <jakedevquestions@gmail.com>
2024-02-12 00:00:58 -0500

    Add settings as global, remove mason-lspconfig (it was choking load times and I wasn't even using it)

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -7,19 +7,9 @@ return {
   },
 
   {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" },
-      })
-    end,
-  },
-
-  {
     "neovim/nvim-lspconfig",
     lazy = false,
-    dependencies = { "folke/neodev.nvim", opts = {} },
+    dependencies = { "folke/neodev.nvim", ft="lua" },
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
