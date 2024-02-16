ae00a8fca3c025b3bc33a23a6dbffced8e2ce19e
Jake Levi <jakedevquestions@gmail.com>
2024-02-15 23:31:31 -0500

    set cmdheight=2

--- lua/options.lua
+++ lua/options.lua
@@ -1,11 +1,12 @@
 vim.o.termguicolors = true
 vim.o.guifont = "Hermit:h10"
+vim.opt.termguicolors = true
+vim.opt.showmode = false
 vim.cmd("set tabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set expandtab")
 vim.cmd("set smartindent")
 vim.cmd("set autochdir")
 vim.cmd("set conceallevel=2")
+vim.cmd("set cmdheight=2")
 vim.cmd("set cursorline")
-
-vim.opt.showmode = false
