62b26f22c1ff587d56bf8b52232f2c116f63abaa
Jake Levi <jakedevquestions@gmail.com>
2024-02-15 23:30:21 -0500

    remove zen and twilight

--- lua/plugins/writing.lua
+++ lua/plugins/writing.lua
@@ -1,17 +1,5 @@
 return -- Lua
 {
-  {
-    "folke/zen-mode.nvim",
-    dependencies = "folke/twilight.nvim",
-    cmd = "ZenMode",
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
-  },
-
-  { "preservim/vim-pencil" },
-  vim.keymap.set("n", "<leader>mz", ":ZenMode<cr>", { desc = "Toggle Mode Zen" }),
-  vim.keymap.set("n", "<leader>mw", ":ZenMode | PencilToggle<cr>", { desc = "Toggle Mode Writing" }),
+  "preservim/vim-pencil",
+  vim.keymap.set("n", "<leader>mw", ":PencilToggle<cr>", { desc = "writing mode toggle" }),
 }
